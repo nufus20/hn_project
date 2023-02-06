a576cd6d754cf98832364b4692208837efc90334
nufus20 <hayatonnufus2020@gmail.com>
2023-02-06 15:15:00 +0700

    membuat form

---
+++ resources/views/pemesanan/form.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.master')
+@section('judul','Tambah Data Pemesanan')
+@section('content-header')
+<section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Tambah Data Pemesanan</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Tambah Data Pemesanan</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+@endsection
+
+@section('content')
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        <form method="POST" action="/pemesanan">
+            @csrf
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Tanggal Pemesanan</label>
+              <input type="text" name="tgl_pemesanan" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Biaya</label>
+              <input type="text" name="biaya" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Id Pembeli</label>
+              <input type="text" name="id_pembeli" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Jumlah</label>
+              <input type="text" name="jumlah" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Id Barang</label>
+              <input type="text" name="id_barang" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Tanggal Selesai</label>
+              <input type="text" name="tgl_selesai" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+          </form>
+      </div>
+      <!-- /.card-body -->
+      
+    </div>
+    <!-- /.card -->
+
+  </section>
+@endsection
