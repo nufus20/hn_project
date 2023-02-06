93afaa98c5ca5f55feafea03ed0bea8de3caf1ab
nufus20 <hayatonnufus2020@gmail.com>
2023-02-06 14:04:47 +0700

    create migration

---
+++ database/migrations/2023_02_06_035248_create_pemesanans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pemesanans', function (Blueprint $table) {
+            $table->id();
+            $table->date('tgl_pemesanan')->unique();
+            $table->string('biaya');
+            $table->string('id_pembeli');
+            $table->string('jumlah');
+            $table->string('id_barang');
+            $table->date('tgl_selesai');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pemesanans');
+    }
+};
